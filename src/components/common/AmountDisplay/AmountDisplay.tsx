7e24cf5a0420dcbe1922f08ae9f5a388c362136d
babamovandrej <andrejbabamov@gmail.com>
2025-04-14 12:45:21 +0200

    Add states to inputs

--- src/components/common/AmountDisplay/AmountDisplay.tsx
+++ src/components/common/AmountDisplay/AmountDisplay.tsx
@@ -6,29 +6,36 @@ import { formatTokenAmount } from '@/utils/tokens'
 import './AmountDisplay.pcss'
 
 export const AmountDisplay: FC = memo(() => {
-	const { fromAmount, isLoading, error } = useFormStore()
+    const { fromAmount, isLoading, error } = useFormStore()
 
-	const displayAmount = useMemo(() => {
-		if (error || !fromAmount || fromAmount === '0') {
-			return '0'
-		}
-		return formatTokenAmount(fromAmount)
-	}, [fromAmount, error])
+    const displayAmount = useMemo(() => {
+        if (error || !fromAmount || fromAmount === '0') {
+            return '0'
+        }
+        return formatTokenAmount(fromAmount)
+    }, [fromAmount, error])
 
-	return (
-		<div className="amount_display">
-			{isLoading ? (
-				<SkeletonLoader height={58} width={128} />
-			) : (
-				<input
-					type="text"
-					className="amount_display_input"
-					placeholder="0"
-					value={displayAmount}
-					readOnly
-					aria-label="Token amount"
-				/>
-			)}
-		</div>
-	)
+    const inputClass = useMemo(() => {
+        const baseClass = "amount_display_input"
+        return displayAmount && displayAmount !== '0' 
+            ? `${baseClass} amount_display_has_value` 
+            : baseClass
+    }, [displayAmount])
+
+    return (
+        <div className="amount_display">
+            {isLoading ? (
+                <SkeletonLoader height={58} width={128} />
+            ) : (
+                <input
+                    type="text"
+                    className={inputClass}
+                    placeholder="0"
+                    value={displayAmount}
+                    readOnly
+                    aria-label="Token amount"
+                />
+            )}
+        </div>
+    )
 })
--- src/components/common/AmountInput/AmountInput.tsx
+++ src/components/common/AmountInput/AmountInput.tsx
@@ -5,29 +5,34 @@ import { useAccount } from 'wagmi'
 import './AmountInput.pcss'
 
 export const AmountInput: FC = memo(() => {
-	const { isConnected } = useAccount()
-	const { value, onChange, onFocus, onBlur } = useHandleInput()
+    const { isConnected } = useAccount()
+    const { value, onChange, onFocus, onBlur } = useHandleInput()
 
-	const containerClass = useMemo(
-		() => (isConnected ? 'amount_input' : 'amount_input amount_input_disabled'),
-		[isConnected],
-	)
+    const containerClass = useMemo(
+        () => (isConnected ? 'amount_input' : 'amount_input amount_input_disabled'),
+        [isConnected],
+    )
 
-	return (
-		<div className={containerClass}>
-			<input
-				type="text"
-				className="amount_input_input"
-				placeholder="0"
-				value={value}
-				onChange={onChange}
-				onFocus={onFocus}
-				onBlur={onBlur}
-				disabled={!isConnected}
-				aria-label="Amount input"
-				inputMode="decimal"
-				autoComplete="off"
-			/>
-		</div>
-	)
+    const inputClass = useMemo(() => {
+        const baseClass = "amount_input_input"
+        return value && value !== '0' ? `${baseClass} amount_input_has_value` : baseClass
+    }, [value])
+
+    return (
+        <div className={containerClass}>
+            <input
+                type="text"
+                className={inputClass}
+                placeholder="0"
+                value={value}
+                onChange={onChange}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                disabled={!isConnected}
+                aria-label="Amount input"
+                inputMode="decimal"
+                autoComplete="off"
+            />
+        </div>
+    )
 })
