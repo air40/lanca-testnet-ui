187e276bb1fea5b6a3a724582f760101546680aa
babamovandrej <andrejbabamov@gmail.com>
2025-04-22 15:25:50 +0200

    Add faucet page description

--- src/pages/Faucet.tsx
+++ src/pages/Faucet.tsx
@@ -2,23 +2,19 @@ import { memo, type FC } from 'react'
 import { Faucet } from '@/components/faucet/Faucet'
 import { MetaTags } from '@/components/common/MetaTags/MetaTags'
 
-const META_TITLE = 'Concero | Testnet'
-const META_DESCRIPTION = 'Welcome to Concero Testnet – a risk-free, developer-focused blockchain sandbox for testing our fully decentralized cross-chain messaging solution featuring secure token swaps and seamless inter-chain communications.'
-const PREFETCH_URL = 'https://api.concero.io'
+const META_TITLE = 'Concero | Faucet List'
+const META_DESCRIPTION =
+	'Welcome to Concero Testnet Faucet Hub – your ultimate source for free testnet tokens across leading networks like Blast, Cronos, Goerli, Sepolia, Polygon, Avalanche and more. Optimize your blockchain testing workflow with our user‑friendly UI, comprehensive network coverage, and secure token bridges'
 
 export const FaucetPage: FC = memo((): JSX.Element => {
-    return (
-        <>
-            <MetaTags 
-                title={META_TITLE} 
-                description={META_DESCRIPTION} 
-                prefetchUrl={PREFETCH_URL} 
-            />
-            <main>
-                <Faucet />
-            </main>
-        </>
-    )
+	return (
+		<>
+			<MetaTags title={META_TITLE} description={META_DESCRIPTION} />
+			<main>
+				<Faucet />
+			</main>
+		</>
+	)
 })
 
 FaucetPage.displayName = 'FaucetPage'
