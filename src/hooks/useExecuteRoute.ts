48934cbecc4075ad00fd38876a864e9ae4776c8b
babamovandrej <andrejbabamov@gmail.com>
2025-06-01 14:24:51 +0200

    Fix wallet client initialization object

--- src/hooks/useExecuteRoute.ts
+++ src/hooks/useExecuteRoute.ts
@@ -1,13 +1,13 @@
 import { useCallback, useRef } from 'react'
-import { getWalletClient } from '@wagmi/core'
-import { adapter } from '@/configuration/wagmi'
 import { IExecutionConfig, IRouteType } from '@lanca/sdk'
 import { useSDK } from './useSDK'
 import { useExecutionListener } from './useExecutionListener'
 import { useTxExecutionStore } from '@/stores/tx-execution/useTxExecutionStore'
+import { useWalletClient } from 'wagmi'
 
 export const useExecuteRoute = (route: IRouteType | null) => {
 	const { setError } = useTxExecutionStore()
+	const { data: client } = useWalletClient()
 	const sdk = useSDK()
 	const updateHandler = useExecutionListener()
 
@@ -18,12 +18,9 @@ export const useExecuteRoute = (route: IRouteType | null) => {
 	return useCallback(async () => {
 		if (!route) return null
 		if (!sdk) throw new Error('SDK not initialized')
+		if (!client) throw new Error('Wallet client not available')
 
 		try {
-			const chainId = Number(route.from.chain.id)
-			const client = await getWalletClient(adapter.wagmiConfig, { chainId })
-
-			// @ts-ignore
 			return await sdk.executeRoute(route, client, configRef.current)
 		} catch (error) {
 			setError(error)
